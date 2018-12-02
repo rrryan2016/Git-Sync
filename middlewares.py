5c172fd08cfbd030b65120469c09cb517095ed77
ghostInSh3ll <15273152851@163.com>
2018-12-02 11:53:37 +0800

    Sunday Review 2

--- middlewares.py
+++ middlewares.py
@@ -21,5 +21,86 @@ DOWNLOADER_MIDDLEWARES = {
   'AdvanceSpider.middlewares.ProxyMiddleware': 543,
 }
 
+// or 
+// 数字越小，越靠近引擎，数字越大越靠近下载器，所以数字越小的，processrequest()优先处理；数字越大的，process_response()优先处理；若需要关闭某个中间件直接设为None即可
+
+DOWNLOADERMIDDLEWARES = {
+    'myproject.middlewares.Custom_A_DownloaderMiddleware': 543,
+    'myproject.middlewares.Custom_B_DownloaderMiddleware': 643,
+    'myproject.middlewares.Custom_B_DownloaderMiddleware': None,
+}
 
 
+
+// New 
+# -*-coding:utf-8-*-
+import random
+from scrapy.downloadermiddlewares.useragent import UserAgentMiddleware
+
+
+class RotateUserAgentMiddleware(UserAgentMiddleware):
+    def __init__(self, user_agent=''):
+        self.user_agent = user_agent
+
+    def process_request(self, request, spider):
+        ua = random.choice(self.user_agent_list)
+        if ua:
+            # print(ua)
+            request.headers.setdefault('User-Agent', ua)
+
+    # the default user_agent_list composes chrome,I E,firefox,Mozilla,opera,netscape
+    # for more user agent strings,you can find it in http://www.useragentstring.com/pages/useragentstring.php
+    user_agent_list = [ \
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1" \
+        "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11", \
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6", \
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6", \
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1", \
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5", \
+        "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5", \
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3", \
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3", \
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3", \
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3", \
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3", \
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3", \
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3", \
+        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3", \
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3", \
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24", \
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
+    ]
+
+
+//New scrapy connect to selenium 
+from scrapy.http import HtmlResponse
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from gp.configs import *
+
+
+class ChromeDownloaderMiddleware(object):
+
+    def __init__(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')  # 设置无界面
+        if CHROME_PATH:
+            options.binary_location = CHROME_PATH
+        if CHROME_DRIVER_PATH:
+            self.driver = webdriver.Chrome(chrome_options=options, executable_path=CHROME_DRIVER_PATH)  # 初始化Chrome驱动
+        else:
+            self.driver = webdriver.Chrome(chrome_options=options)  # 初始化Chrome驱动
+
+    def __del__(self):
+        self.driver.close()
+
+    def process_request(self, request, spider):
+        try:
+            print('Chrome driver begin...')
+            self.driver.get(request.url)  # 获取网页链接内容
+            return HtmlResponse(url=request.url, body=self.driver.page_source, request=request, encoding='utf-8',
+                                status=200)  # 返回HTML数据
+        except TimeoutException:
+            return HtmlResponse(url=request.url, request=request, encoding='utf-8', status=500)
+        finally:
+            print('Chrome driver end...')
